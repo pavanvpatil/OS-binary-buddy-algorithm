50164375cd7f9fb78e8688d6cab9c376af5d50cf
pavanvpatil <pavanvpatil01@gmail.com>
2023-04-06 02:18:34 +0530

    done with algo

--- binary_buddy_algo.cpp
+++ binary_buddy_algo.cpp
@@ -1,20 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-class Tree_Node{
-    public:
+class Tree_Node
+{
+public:
     long long int size;
     long long int actual_size;
     char name;
     long long int id;
     bool is_free;
     bool is_divided;
-    long long int counter;    
+    long long int counter;
     Tree_Node *left;
     Tree_Node *right;
     Tree_Node *parent;
-    Tree_Node(long long int size,long long int actual_size, char name, long long int id){
+    Tree_Node(long long int size, long long int actual_size, char name, long long int id)
+    {
         this->size = size;
         this->actual_size = actual_size;
         this->name = name;
@@ -28,43 +30,42 @@ class Tree_Node{
     }
 };
 
-void find_best_fit(Tree_Node *root, long long int size, Tree_Node *best_fit_node)
+void find_best_fit(Tree_Node *root, long long int size, Tree_Node **best_fit_node)
 {
-    cout<< "Finding best fit for " << size << endl;
-    if(root == NULL)
+    if (root == NULL)
         return;
     find_best_fit(root->left, size, best_fit_node);
-    if(root->is_free && root->size >= size){
-        cout << "Found free node: " << root->size << endl;
-        if(best_fit_node == NULL)
-            best_fit_node = root;
-        else if(root->size < best_fit_node->size)
-            best_fit_node = root;
-        else if(root->size == best_fit_node->size && root->counter < best_fit_node->counter)
-            best_fit_node = root;
+    if (root->is_free && root->size >= size)
+    {
+        if ((*best_fit_node) == NULL)
+            *best_fit_node = root;
+        else if (root->size < (*best_fit_node)->size)
+            *best_fit_node = root;
+        else if (root->size == (*best_fit_node)->size && root->counter < (*best_fit_node)->counter)
+            *best_fit_node = root;
     }
     find_best_fit(root->right, size, best_fit_node);
 }
 
-void allocate_memory(Tree_Node *root, long long int size, char name, long long int& id,long long int lower_limit){
-    cout<< "Allocating memory for " << name << " of size " << size << endl;
-    Tree_Node* best_fit_node;
-    find_best_fit(root, size, best_fit_node);
+void allocate_memory(Tree_Node *root, long long int size, char name, long long int &id, long long int lower_limit)
+{
+    Tree_Node *best_fit_node = NULL;
+    find_best_fit(root, size, &best_fit_node);
 
-    cout << "Best fit node: " << best_fit_node->size << endl;
-
-    while(true)
+    while (true)
     {
-        if(best_fit_node->size/2 >= size && best_fit_node->size/2 >= lower_limit){
+        if (best_fit_node->size / 2 >= size && best_fit_node->size / 2 >= lower_limit)
+        {
             best_fit_node->is_divided = true;
             best_fit_node->is_free = false;
-            best_fit_node->left = new Tree_Node(best_fit_node->size/2, best_fit_node->actual_size/2, '.', ++id);
-            best_fit_node->right = new Tree_Node(best_fit_node->size/2, best_fit_node->actual_size/2, '.', ++id);
+            best_fit_node->left = new Tree_Node(best_fit_node->size / 2, best_fit_node->actual_size / 2, '.', ++id);
+            best_fit_node->right = new Tree_Node(best_fit_node->size / 2, best_fit_node->actual_size / 2, '.', ++id);
             best_fit_node->left->parent = best_fit_node;
             best_fit_node->right->parent = best_fit_node;
             best_fit_node = best_fit_node->left;
         }
-        else{
+        else
+        {
             best_fit_node->is_free = false;
             best_fit_node->name = name;
             best_fit_node->id = ++id;
@@ -72,20 +73,21 @@ void allocate_memory(Tree_Node *root, long long int size, char name, long long i
             break;
         }
     }
-
 }
 
-void free_memory(Tree_Node* root, char name)
+void free_memory(Tree_Node *root, char name)
 {
-    if(root == NULL)
+    if (root == NULL)
         return;
     free_memory(root->left, name);
-    if(root->name == name){
+    if (root->name == name)
+    {
         root->is_free = true;
         root->name = '.';
         root->id = 0;
         root->actual_size = 0;
-        if(root->parent != NULL && root->parent->left->is_free && root->parent->right->is_free){
+        if (root->parent != NULL && root->parent->left->is_free && root->parent->right->is_free)
+        {
             root->parent->is_divided = false;
             root->parent->is_free = true;
             root->parent->counter = 0;
@@ -101,54 +103,65 @@ void free_memory(Tree_Node* root, char name)
     free_memory(root->right, name);
 }
 
-void increase_counter_all(Tree_Node* root)
+void increase_counter_all(Tree_Node *root)
 {
-    if(root==NULL)
+    if (root == NULL)
         return;
     increase_counter_all(root->left);
-    if(root->is_free)
+    if (root->is_free)
         root->counter++;
     increase_counter_all(root->right);
 }
 
-void print_leaf_nodes(Tree_Node* root)
+void print_leaf_nodes(Tree_Node *root)
 {
-    if(root == NULL)
+    if (root == NULL)
         return;
     print_leaf_nodes(root->left);
-    if(root->left == NULL && root->right == NULL)
+    if (root->left == NULL && root->right == NULL)
     {
-        if(root->is_free)
-            cout << "Free Block: "<< root->size << endl;
+        if (root->is_free)
+            cout << "Free Block: " << root->size << endl;
         else
-            cout << root->name << ": "<< root->actual_size << endl;
+            cout << root->name << ": " << root->actual_size << endl;
     }
     print_leaf_nodes(root->right);
 }
 
-int main()
+int main(int argc, char const *argv[])
 {
-    ifstream infile("input.txt");
+    if(argc != 2)
+    {
+        cout << "Usage: ./a.out <input_file>" << endl;
+        return 0;
+    }
+
+    ifstream infile(argv[1]);
+    if(!infile.is_open())
+    {
+        cout << "Error in opening file" << endl;
+        return 0;
+    }
+
     int no_of_test_cases;
     infile >> no_of_test_cases;
     string read_line;
     getline(infile, read_line); // to read the empty line
 
-    while(no_of_test_cases--){
-        long long int upper_limit,lower_limit;
+    while (no_of_test_cases--)
+    {
+        long long int upper_limit, lower_limit;
         infile >> upper_limit >> lower_limit;
 
         upper_limit = pow(2, upper_limit);
         lower_limit = pow(2, lower_limit);
-        
 
-        getline(infile, read_line);// to read the empty line
+        getline(infile, read_line); // to read the empty line
         getline(infile, read_line); // to read the empty line
 
-        vector<pair<char,int>> requests;
-        while(getline(infile, read_line) && read_line != "")
+        vector<pair<char, int>> requests;
+        while (getline(infile, read_line) && read_line != "")
         {
-            cout<< read_line << endl;
             char name;
             long long int size;
             sscanf(read_line.c_str(), "%c %llu", &name, &size);
@@ -156,12 +169,14 @@ int main()
         }
         long long int id = 0;
 
-        Tree_Node *root = new Tree_Node(upper_limit, upper_limit,'.', id);
-        
-        for(auto request: requests){
+        Tree_Node *root = new Tree_Node(upper_limit, upper_limit, '.', id);
+
+        for (auto request : requests)
+        {
             char name = request.first;
             long long int size = request.second;
-            if(size == 0){
+            if (size == 0)
+            {
                 free_memory(root, name);
             }
             else
@@ -171,7 +186,9 @@ int main()
             increase_counter_all(root);
         }
 
+        cout << "Case " << no_of_test_cases + 1 << ":" << endl;
         print_leaf_nodes(root);
+        cout << endl;
     }
     return 0;
 }
